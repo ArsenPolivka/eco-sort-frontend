b92e8c2080867c5affecbb193116998b1195b243
Арсен Полівка <Arsen.Polivka@lnu.edu.ua>
2023-05-22 03:10:54 +0300

    tech/code refactored

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import 'normalize.css'
 
 export default function Home() {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey: "AIzaSyCtSYwNr8WCyxcAc9vLQF6qgJ63Op5EaNc",
   });
 
   if (!isLoaded) {
